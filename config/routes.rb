3bf6a41c503447447a5fdf5bcbd009be2d293646
Travis Anderson <31084612+Travis-Anderson83@users.noreply.github.com>
2017-10-28 17:23:14 +1000

    initial commit and bootstrap navbar complete

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  resources :books
+  root 'pages#index'
+  get 'pages/about', to: 'pages#about'
+
+
+
+  get 'pages/contact', to: 'pages#contact'
+
+  get 'pages/location', to: 'pages#location'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
