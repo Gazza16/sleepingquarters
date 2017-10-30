07869c1015f0560013cfa7167435b60c57665e2c
Travis Anderson <31084612+Travis-Anderson83@users.noreply.github.com>
2017-10-30 15:16:32 +1000

    add user name

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
   address:                'smtp.sendgrid.net',
   port:                   '587',
   authentication:         :plain,
-  user_name:              ENV.fetch('SENDGRID_USER'),
+  user_name:              ENV.fetch('SENDGRID_USERNAME'),
   password:               ENV.fetch('SENDGRID_PASSWORD'),
   domain:                 'https://rocky-atoll-94227.herokuapp.com',
   enable_starttls_auto:   true
