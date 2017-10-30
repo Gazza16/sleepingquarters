f8dbb3f807960fbe854b08f0c831439fdfd5c2f5
Travis Anderson <31084612+Travis-Anderson83@users.noreply.github.com>
2017-10-30 15:04:21 +1000

    more config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { :host => 'https://rocky-atoll-94227.herokuapp.com/' }
+}
 
 
   config.paperclip_defaults = {
