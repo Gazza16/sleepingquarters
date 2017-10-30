f2979cb1b38b98da4b7190c3fda9dedf0bfc6638
Travis Anderson <31084612+Travis-Anderson83@users.noreply.github.com>
2017-10-30 13:03:16 +1000

    development email set up

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,42 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  #Get /users
+  #Get /users.json
+  def index
+    @users = User.all
+  end
+
+  # # GET /users/1
+ # # GET /users/1.json
+  def show
+
+  end
+  # get users/1/edit
+  def edit
+
+  end
+
+  # # PATCH/PUT /users/1
+  # # PATCH/PUT /users/1.json
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: "User was successfully updated." }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json {render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:role, :user_name)
+  end
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,5 +62,18 @@ Rails.application.configure do
     s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com"
   }
 }
-
+#for devise signup emails
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = true
+config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  user_name:      ENV['SENDMAIL_USERNAME'],
+  password:       ENV['SENDMAIL_PASSWORD'],
+  domain:         ENV['MAIL_HOST'],
+  address:       'smtp.gmail.com',
+  port:          '587',
+  authentication: :plain,
+  enable_starttls_auto: true
+}
 end
