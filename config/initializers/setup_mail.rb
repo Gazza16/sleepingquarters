884bf42f7da81e8e335122dddb0820188728e2c6
Travis Anderson <31084612+Travis-Anderson83@users.noreply.github.com>
2017-10-30 14:01:37 +1000

    finetuning vars and apis

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,10 +1,10 @@
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-  :address                'smtp.sendgrid.net',
-  :port                   '587',
-  :authentication         :plain,
-  :user_name              ENV['HEROKU_USER'],
-  :password               ENV['HEROKU_PASSWORD'],
-  :domain                 'heroku.com',
-  :enable_starttls_auto   true
+  address:                'smtp.sendgrid.net',
+  port:                   '587',
+  authentication:         :plain,
+  user_name:              ENV['SENDGRID_USER'],
+  password:               ENV['SENDGRID_PASSWORD'],
+  domain:                 'heroku.com',
+  enable_starttls_auto:   true
 }
