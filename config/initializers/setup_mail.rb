55b2bb1915af0cae5044c5866a1ff56a04cef634
Travis Anderson <31084612+Travis-Anderson83@users.noreply.github.com>
2017-10-30 15:14:01 +1000

    Change to .fetch

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,8 +3,8 @@ ActionMailer::Base.smtp_settings = {
   address:                'smtp.sendgrid.net',
   port:                   '587',
   authentication:         :plain,
-  user_name:              'app79893154@heroku.com',
-  password:               'oratvnlt1024',
+  user_name:              ENV.fetch('SENDGRID_USER'),
+  password:               ENV.fetch('SENDGRID_PASSWORD'),
   domain:                 'https://rocky-atoll-94227.herokuapp.com',
   enable_starttls_auto:   true
 }
