4a60fe9e01e32af3cfc697e9463fbca9107331be
Travis Anderson <31084612+Travis-Anderson83@users.noreply.github.com>
2017-10-31 11:18:51 +1000

    add user to boats

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171030053442) do
+ActiveRecord::Schema.define(version: 20171031003328) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 20171030053442) do
     t.string "image_content_type"
     t.integer "image_file_size"
     t.datetime "image_updated_at"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_boats_on_user_id"
   end
 
   create_table "books", force: :cascade do |t|
@@ -66,4 +68,5 @@ ActiveRecord::Schema.define(version: 20171030053442) do
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
+  add_foreign_key "boats", "users"
 end
