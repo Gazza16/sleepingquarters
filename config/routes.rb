7342979ccdf9f01363e6b91db97d396b1358b7d2
Travis Anderson <31084612+Travis-Anderson83@users.noreply.github.com>
2017-11-03 14:06:03 +1000

    first phase messaging set up

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,21 @@ Rails.application.routes.draw do
   resources :bookings
   get 'users/index'
   get 'users/edit/:id', to: 'users#edit'
-  # get 'boats/show/:id', to: 'boats#show'
   get 'users/show'
   post 'boats/:id', to: 'bookings#create'
-  resources :boats
-  devise_for :users
-  resources :books
   root 'pages#index'
   get 'pages/about', to: 'pages#about'
   get 'pages/contact', to: 'pages#contact'
   get 'pages/location', to: 'pages#location'
+
+  resources :boats
+  devise_for :users
+  resources :books
+
+  resources :conversations do
+    resources :messages
+
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
